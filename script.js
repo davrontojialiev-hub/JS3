b25740d1274a320975440a567f6470bd5fec765f
davron <dav@t>
2026-03-02 00:28:13 +0500

    adapted js

---
+++ script.js
@@ -0,0 +1,13 @@
+let number = +(prompt("Введите любое число:"));
+
+for (let i = 1; i <= number; i++) {
+    if (i === 1) {
+        console.log(i + " banana");
+    } else {
+        console.log(i + " bananas");
+    }
+}
+
+let num = +(prompt("Введите любое число:"));
+for (let i = 2; i <= num; i += 2) {
+    }
