0b0d603d4a3b6bc614fc8ae0121e2c010cbbcbf5
davron <dav@t>
2026-03-04 20:10:07 +0500

    adapted js

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ for (let i = 1; i <= number; i++) {
     }
 }
 
-let num = +(prompt("Введите любое число:"));
-for (let i = 2; i <= num; i += 2) {
-    }
+ let num = +(prompt("Введите сколько раз сработает цикл:"));
+ let sum = 0;
+   for (let i = 2; i < num; i += 2) {
+    sum += i;   
+ }
+ console.log("Сумма всех четных чисел:" + sum);
+ 
+
+ let num1 = +(prompt("Введите любое число:"));
+ let degree = +(prompt("Введите степень:"));
+
+console.log("ответ: " + Math.pow(num1, degree));
